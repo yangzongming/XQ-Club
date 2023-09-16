3c0487698e0efbfb502645109e21bcd1b35e3218
laoyang <lao_yang_cool@126.com>
2023-09-16 23:16:27 +0800

    fix logic

--- backend/conf/env.py
+++ backend/conf/env.py
@@ -7,12 +7,12 @@ from application.settings import BASE_DIR
 # ================================================= #
 # 数据库 ENGINE ，默认演示使用 sqlite3 数据库，正式环境建议使用 mysql 数据库
 # sqlite3 设置
-DATABASE_ENGINE = "django.db.backends.sqlite3"
-DATABASE_NAME = os.path.join(BASE_DIR, "db.sqlite3")
+#DATABASE_ENGINE = "django.db.backends.sqlite3"
+#DATABASE_NAME = os.path.join(BASE_DIR, "db.sqlite3")
 
 # 使用mysql时，改为此配置
-# DATABASE_ENGINE = "django.db.backends.mysql"
-# DATABASE_NAME = 'django-vue-admin' # mysql 时使用
+DATABASE_ENGINE = "django.db.backends.mysql"
+DATABASE_NAME = 'django-vue-admin' # mysql 时使用
 
 # 数据库地址 改为自己数据库地址
 DATABASE_HOST = "127.0.0.1"
