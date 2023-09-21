596620f2040a3d7928331608220516a89d2a079d
laoyang <lao_yang_cool@126.com>
2023-09-21 19:49:15 +0800

    fix

--- backend/dvadmin/system/views/xingqi/xingqi_erp.py
+++ backend/dvadmin/system/views/xingqi/xingqi_erp.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request,name,age):
-    html = '<h1>星奇测试------ Leo Hello World， I am Django。</h1>' + name + age
+def index(request):
+    html = '<h1>星奇测试------ Leo Hello World， I am Django。</h1>'
     return HttpResponse(html, status=200)
