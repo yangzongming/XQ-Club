ab14e5766afeb98532cdf7b7109264deaf2373cb
laoyang <lao_yang_cool@126.com>
2023-09-21 12:16:45 +0800

    fix

--- backend/dvadmin/system/views/xingqi/xingqi_erp.py
+++ backend/dvadmin/system/views/xingqi/xingqi_erp.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    html = '<h1>Hello World</h1>'
+    html = '<h1>星奇测试------ Leo Hello World， I am Django。</h1>'
     return HttpResponse(html, status=200)
