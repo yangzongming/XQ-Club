441978a21f9a3dfc7ca65ed22c28d0d7d5b354e9
laoyang <lao_yang_cool@126.com>
2023-09-23 21:24:13 +0800

    fix

--- backend/dvadmin/system/views/xingqi/xingqi_erp.py
+++ backend/dvadmin/system/views/xingqi/xingqi_erp.py
@@ -26,8 +26,8 @@ def testJson(request):
     return response_page_success(message="成功了", data=dlist, total=100, limit= 10, page= 1)
 
 def get_purcharse_track_detail(request):
-    dlist = craw_purcharse_detail()
-    return response_page_success(message="成功了", data=dlist, total=100, limit= 10, page= 1)
+    dic = craw_purcharse_detail()
+    return response_page_success(message="成功了", data=dic["data"], total=dic["total"], limit= dic["limit"], page= dic["page"])
 
 def craw_purcharse_detail():
     print("123")
@@ -206,9 +206,112 @@ def craw_purcharse_detail():
 	    AND STKIE.[【采购入库】物料编码ID] = purrey.[【收料通知单】物料ID] """
     recSet = cursor.execute(sql)
     datalist = recSet.fetchall()
-    for line in datalist:
-        print(line)
     xls_lines = []
+    for line in datalist:
+        xls_lines.append({
+            '[采购申请]单号': line[0],
+            '[采购申请]归属项目': line[1],
+            '[采购申请]申请人': line[2],
+            '[采购申请]单据状态': line[3],
+            '[采购申请]创建日期': line[4],
+            '[采购申请]审核日期': line[5],
+            '[采购申请]申请日期': line[6],
+            '[采购申请]单据类型': line[7],
+            '[采购申请]物料ID': line[8],
+            '[采购申请]物料编码': line[9],
+            '[采购申请]物料名称': line[10],
+            '[采购申请]规格型号': line[11],
+            '[采购申请]申请数量': line[12],
+            '[采购申请]批准数量': line[13],
+            '[采购申请]行业务关闭': line[14],
+            '[采购申请]要求到货日期': line[15],
+            '[采购申请]剩余数量': line[16],
+            '[采购申请]订单数量': line[17],
+            '[采购申请]入库数量（几本单位）': line[18],
+            '[采购申请]收料数量（几本单位）': line[19],
+            '[采购订单]编号': line[20],
+            '[采购订单]单据状态': line[21],
+            '[采购订单]采购日期': line[22],
+            '[采购订单]供应商': line[23],
+            '[采购订单]创建人': line[24],
+            '[采购订单]创建日期': line[25],
+            '[采购订单]审核日期': line[26],
+            '[采购订单]采购员': line[27],
+            '[采购订单]单据类型': line[28],
+            '[采购订单]确认日期': line[29],
+            '[采购订单]ID': line[30],
+            '[采购订单]采购数量': line[31],
+            '[采购订单]行业务关闭': line[32],
+            '[采购订单]要求交货日期': line[33],
+            '[采购订单]供应商承诺日期': line[34],
+            '[采购订单]累计收料数量': line[35],
+            '[采购订单]累计入库数量': line[36],
+            '[采购订单]剩余收料数量': line[37],
+            '[采购订单]剩余入库数量': line[38],
+            '[采购订单]源单类型': line[39],
+            '[采购订单]原单编号': line[40],
+            '[采购订单]需求来源': line[41],
+            '[采购订单]需求单据编号': line[42],
+            '[采购订单]需求单据行号': line[43],
+            '[采购订单]需求单据分录内码': line[44],
+            '[收料通知单]编号': line[45],
+            '[收料通知单]单据状态': line[46],
+            '[收料通知单]创建人': line[47],
+            '[收料通知单]创建日期': line[48],
+            '[收料通知单]审核日期': line[49],
+            '[收料通知单]审核人': line[50],
+            '[收料通知单]ID': line[51],
+            '[收料通知单]物料ID': line[52],
+            '[收料通知单]本次交货数量': line[53],
+            '[收料通知单]实到数量': line[54],
+            '[收料通知单]订单单号': line[55],
+            '[收料通知单]原单内码': line[56],
+            '[收料通知单]原单分录内码': line[57],
+            '[收料通知单]原单单号': line[58],
+            '[收料通知单]实收数量': line[59],
+            '[收料通知单]预计到货日期': line[60],
+            '[收料通知单]采购订单分录代码': line[61],
+            '[收料通知单]确认交货数量': line[62],
+            '[收料通知单]确认到货日期': line[63],
+            '[检验单]单号': line[64],
+            '[检验单]单据状态': line[65],
+            '[检验单]审核人': line[66],
+            '[检验单]审核日期': line[67],
+            '[检验单]创建人': line[68],
+            '[检验单]创建日期': line[69],
+            '[检验单]单据日期': line[70],
+            '[检验单]检验数量': line[71],
+            '[检验单]质检开始日期': line[72],
+            '[检验单]质检结束日期': line[73],
+            '[检验单]物料编码ID': line[74],
+            '[检验单]原单类型': line[75],
+            '[检验单]原单编号': line[76],
+            '[检验单]原单内码': line[77],
+            '[检验单]原单分录内码': line[78],
+            '[检验单]原单行号': line[79],
+            '[采购入库]单号': line[80],
+            '[采购入库]单据状态': line[81],
+            '[采购入库]入库日期': line[82],
+            '[采购入库]单据类型': line[83],
+            '[采购入库]供应商': line[84],
+            '[采购入库]创建人': line[85],
+            '[采购入库]创建日期': line[86],
+            '[采购入库]审核人': line[87],
+            '[采购入库]审核日期': line[88],
+            '[采购入库]物料编码ID': line[89],
+            '[采购入库]应收数量': line[90],
+            '[采购入库]实收数量': line[91],
+            '[采购入库]原单行内码': line[92],
+            '[采购入库]订单单号': line[93],
+            '[采购入库]原单类型': line[94],
+            '[采购入库]原单编码': line[95],
+        })
+    return {
+        "data": xls_lines,
+        "total": len(xls_lines),
+        "limit": 100,
+        "page": 1,
+    }
 
 #获取采购申请表明细
 def craw_requestion_detail(fid):
