d1566f4d158e0d5391873e8c1b593bcfafb71b88
laoyang <lao_yang_cool@126.com>
2023-09-23 22:22:18 +0800

    fix

--- backend/dvadmin/utils/DateEncode.py
+++ backend/dvadmin/utils/DateEncode.py
@@ -1,9 +1,13 @@
 import datetime
 import json
+import decimal
+
 
 class DateEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj,datetime.datetime):
             return obj.strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(obj, decimal.Decimal):
+            return str(decimal.Decimal(obj).quantize(decimal.Decimal('0.00')))
         else:
             return json.JSONEncoder.default(self,obj)
