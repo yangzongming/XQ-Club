e8628ed6f0ebf912103db76acb6c815c39768c9c
laoyang <lao_yang_cool@126.com>
2023-10-11 22:39:37 +0800

    fix

--- web/src/api/upload.js
+++ web/src/api/upload.js
@@ -2,7 +2,7 @@
  * “上传文件”相关接口
  */
 import { request } from '@/api/service'
-import { createRequestFunctionSELF } from '@/api/service'
+import { requestForOutside_SELF } from '@/api/service'
 import axios from 'axios'
 
 const urlPre = 'http://172.17.1.249:9602/api/system/xingqi'
@@ -14,7 +14,7 @@ export default {
     const config = {
       headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
     }
-    let service = createRequestFunctionSELF()
+    let service = requestForOutside_SELF()
     console.log("fuck")
     //console.log(Object.prototype.toString.call(service))
     service.post(urlPre + "/upload_file", formData, config)
