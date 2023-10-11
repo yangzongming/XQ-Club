e5fd067ca3eac1abf143301ed4376b0af0db1587
laoyang <lao_yang_cool@126.com>
2023-10-11 22:04:51 +0800

    fix

--- web/src/api/upload.js
+++ web/src/api/upload.js
@@ -3,21 +3,23 @@
  */
 import { request } from '@/api/service'
 import { serviceForOutside } from '@/api/service'
+import axios from 'axios'
 
 const urlPre = 'http://172.17.1.249:9602/api/system/xingqi'
 
 export default {
   uploadFile (file) {
-  let formData = new FormData()
-  formData.append('files', file)
-  const config = {
-    headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
-  };
-  let service = serviceForOutside()
+    let formData = new FormData()
+    formData.append('files', file)
+    const config = {
+      headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
+    }
+    let service = serviceForOutside()
     console.log("fuck")
-    console.log(typeof service)
-    service.post(urlPre + "/file/upload",formData,config).then((response) =>{
-     console.log('全部响应结果:',response);
-    })
+    console.log(instanceof axios)
+    service.post(urlPre + "/file/upload", formData, config)
+      .then( response =>{
+        console.log('全部响应结果:', response);
+      })
   }
 }
