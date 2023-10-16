23e8ab281a64dd2a27d98de35b6e70c48738b254
laoyang <lao_yang_cool@126.com>
2023-10-16 23:38:26 +0800

    fix

--- web/src/api/upload.js
+++ web/src/api/upload.js
@@ -16,8 +16,7 @@ export default {
     formData.append('file', file)
     axios.post(urlPre + "/upload_file", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
-          'enctype': 'multipart/form-data'
+          'Content-Type': 'multipart-formData',
         }
       })
   }
