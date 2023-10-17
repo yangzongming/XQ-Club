85003bde79c68df90d51094bacc30e65cf0b29df
laoyang <lao_yang_cool@126.com>
2023-10-17 19:20:11 +0800

    fix

--- web/src/views/xingqi/test2/index.vue
+++ web/src/views/xingqi/test2/index.vue
@@ -5,9 +5,8 @@
             ref="upload"
             action=""
             accept=".xlsx,.xls"
-            on-change="upload_File"
-          <el-button size="small" type="primary">导入</el-button>
-          >
+            on-change="upload_File">
+            <el-button size="small" type="primary">导入</el-button>
     </el-upload>
     <!--列表-->
     <div style="margin-bottom: 30px;"></div>
