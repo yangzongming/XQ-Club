0441eba0ff82c9dde9aea1765e6437859318cfc8
laoyang <lao_yang_cool@126.com>
2023-10-17 19:16:03 +0800

    fix

--- web/src/api/upload.js
+++ web/src/api/upload.js
@@ -1,11 +1,7 @@
 /**
  * “上传文件”相关接口
  */
-import {uploadFile, request, requestForOutside} from '@/api/service'
-//import { requestForOutsideSELF } from '@/api/service'
 import axios from 'axios'
-import util from '@/libs/util'
-import {urlPrefix} from "@/views/system/user/api";
 
 const urlPre = 'http://172.17.1.249:9602/api/system/xingqi'
 
