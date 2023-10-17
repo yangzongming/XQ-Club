794da0e676ee74891f2ab7cf5ea95f65cbbf0dc3
laoyang <lao_yang_cool@126.com>
2023-10-17 19:13:29 +0800

    fix

--- web/src/views/xingqi/test2/index.vue
+++ web/src/views/xingqi/test2/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="app-container">
     <div style="margin-bottom: 30px;"></div>
-    <el-upload
-            ref="upload"
-            action=""
-            accept=".xlsx,.xls"
-            on-change="upload_File"
-          <el-button size="small" type="primary">导入</el-button>
-          >
+    <el-upload>
     </el-upload>
     <!--列表-->
     <div style="margin-bottom: 30px;"></div>
