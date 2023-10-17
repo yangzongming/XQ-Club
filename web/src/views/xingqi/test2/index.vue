b04e8a62312fcc6a386f288078056dc71973d4c7
laoyang <lao_yang_cool@126.com>
2023-10-17 19:35:55 +0800

    fix

--- web/src/views/xingqi/test2/index.vue
+++ web/src/views/xingqi/test2/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="app-container">
     <div style="margin-bottom: 30px;"></div>
+    <el-upload
+      <el-button size="small" type="primary">导入</el-button>
+    </el-upload>
     <!--列表-->
     <div style="margin-bottom: 30px;"></div>
   </div>
 </template>
 <script>
+  import upload from '@/api/upload'
+  export default{
+    methods:{
+      upload_File(file, filelist) {
+         upload.uploadFile(file.raw)
+      }
+    }
+  }
 </script>
