03eed1226c2944987341e6be92ad709e05547a75
laoyang <lao_yang_cool@126.com>
2023-10-22 22:30:26 +0800

    fix

--- backend/dvadmin/system/apps.py
+++ backend/dvadmin/system/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 
 
+from backend.dvadmin.system.views.fastorm import orm
+
 class SystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dvadmin.system'
 
     def ready(self):
-        print("FUCK")
+        import asyncio
+        loop = asyncio.get_event_loop()
+        yield from orm.create_pool(loop=loop)
