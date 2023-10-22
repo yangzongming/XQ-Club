18669b80ee7f52c041d948e3cad07cbbfe356705
laoyang <lao_yang_cool@126.com>
2023-10-22 23:16:17 +0800

    fix

--- backend/dvadmin/system/apps.py
+++ backend/dvadmin/system/apps.py
@@ -9,11 +9,12 @@ class SystemConfig(AppConfig):
     name = 'dvadmin.system'
 
     @asyncio.coroutine
-    def init1(loop):
+    def initA(loop):
         yield from orm.create_pool(loop=loop)
         return 1
 
     def ready(self):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(yield from orm.create_pool(loop=loop))
+
+        loop.run_until_complete(initA(loop))
         loop.run_forever()
