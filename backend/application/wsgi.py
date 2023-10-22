c0570d3f794da3b876d7fcd00b4dc7b359b6a749
laoyang <lao_yang_cool@126.com>
2023-10-22 22:27:33 +0800

    fix

--- backend/application/wsgi.py
+++ backend/application/wsgi.py
@@ -15,7 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 application = get_wsgi_application()
-
-
-def ready(self):
-    print("FUCK")
