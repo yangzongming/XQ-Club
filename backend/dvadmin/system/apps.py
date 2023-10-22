30a2fef14055b89499fef69e1ca22ca0190feec6
laoyang <lao_yang_cool@126.com>
2023-10-22 22:34:09 +0800

    fix

--- backend/dvadmin/system/apps.py
+++ backend/dvadmin/system/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-from fastorm import orm
+from .views.fastorm import orm
 
 class SystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
