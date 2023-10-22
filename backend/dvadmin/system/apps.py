80c403c73cbb3151a8e3f52357653f84ce515fe7
laoyang <lao_yang_cool@126.com>
2023-10-22 23:17:28 +0800

    fix

--- backend/dvadmin/system/apps.py
+++ backend/dvadmin/system/apps.py
@@ -8,13 +8,12 @@ class SystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dvadmin.system'
 
-    @asyncio.coroutine
-    def initA(loop):
-        yield from orm.create_pool(loop=loop)
-        return 1
-
     def ready(self):
         loop = asyncio.get_event_loop()
-
         loop.run_until_complete(initA(loop))
         loop.run_forever()
+
+        @asyncio.coroutine
+        def initA(loop):
+            yield from orm.create_pool(loop=loop)
+            return 1
