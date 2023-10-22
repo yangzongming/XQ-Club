5dc19394e5bf22b96ce9695a2888e29075fa0b29
laoyang <lao_yang_cool@126.com>
2023-10-22 22:40:32 +0800

    fix

--- backend/dvadmin/system/views/fastorm/orm_start.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from backend.dvadmin.system.views.fastorm import orm
-
-
-def start():
-    print(123)
-    loop = asyncio.get_event_loop()
-    yield from orm.create_pool(loop=loop)
