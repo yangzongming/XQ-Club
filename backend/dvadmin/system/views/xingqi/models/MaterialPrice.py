48423480fde82debc2e48f07258ba8a959ec9da0
laoyang <lao_yang_cool@126.com>
2023-10-24 21:19:38 +0800

    fix logic

---
+++ backend/dvadmin/system/views/xingqi/models/MaterialPrice.py
@@ -0,0 +1,44 @@
+from sqlalchemy.ext.declarative import declarative_base    #导入declarative_base()函数来创建基类
+from sqlalchemy import create_engine, Column, Integer, String, Date, Double
+from sqlalchemy.orm import sessionmaker  #导入sessionmaker类函数
+from dvadmin.system.util.sql_config import db_info
+
+engine = create_engine(db_info)      #创建引擎
+base = declarative_base(engine)     #使用declarative_base()函数来创建SQLORM基类
+session = sessionmaker(engine)()    #构建session对象
+
+
+class MaterialPrice(base):
+    __tablename__ = 'material_price'
+    id = Column(Integer, primary_key=True)
+    material_id = Column(Integer)
+    price = Column(Double)
+    amount = Column(Integer)
+    supplier = Column(String(50))
+    creator = Column(String(50))  # 创建者
+    info = Column(String(50))
+    xingqi_number = Column(String(50))
+    create_time = Column(Date)
+    modify_time = Column(Date)
+
+base.metadata.create_all()
+
+
+"""
+DROP TABLE material_price
+CREATE TABLE `material_price` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `material_id` int(10) unsigned NOT NULL,
+  `price` double unsigned DEFAULT '0',
+  `amount` int(10) unsigned NOT NULL,
+  `supplier` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `creator` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `xingqi_number` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `info` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modify_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+SHOW CREATE TABLE `material_price`;
+
+"""
--- backend/dvadmin/system/views/xingqi/xingqi_erp.py
+++ backend/dvadmin/system/views/xingqi/xingqi_erp.py
@@ -26,7 +26,6 @@ def testJson(request):
     material = Material(material_number="UJR-6.35MS-L28-AW", material_name=u'1/4VCR短格兰L28', material_brand="FUJIKIN", info="没有描述")
     session.add(material)
     session.commit()
-
     r1 = session.query(Material).filter(Material.material_number == "UJR-6.35MS-L28-AW").all()
     print(r1)
     return response_page_success(message="成功了", data="", total=100, limit= 10, page= 1)
