a7bcb44176bffff56d5da2f7cc0eb638e30e821d
laoyang <lao_yang_cool@126.com>
2023-10-24 21:54:58 +0800

    fix logic

--- web/src/views/xingqi/MaterialPrice/uploadprice.vue
+++ web/src/views/xingqi/MaterialPrice/uploadprice.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
+      <div style="margin-bottom: 30px;"></div>
       <span>这个功能是为了收集星奇采购部零散的询价单</span>
       <el-divider></el-divider>
+
+      <el-upload :on-change="upload_File">
+        <el-button size="small" type="primary">上传询价单</el-button>
+      </el-upload>
     <div>
     <el-table
       :data="tableData"
@@ -24,4 +29,12 @@
 </template>
 
 <script>
+import upload from '@/api/upload'
+  export default{
+    methods:{
+      upload_File(file, filelist) {
+        upload.uploadFile(file.raw)
+      }
+    }
+  }
 </script>
