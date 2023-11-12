4e8a76273a31ffb947f22d9524263d6717b513f3
laoyang <lao_yang_cool@126.com>
2023-11-12 16:38:29 +0800

    fix logic

--- backend/dvadmin/system/views/xingqi/util/QuoteUtil.py
+++ backend/dvadmin/system/views/xingqi/util/QuoteUtil.py
@@ -49,10 +49,13 @@ def handleMaterialPrice(filename, file_md5):
             "price": (sheet1.cell(row=row, column=14)).value,
         }
         material_list.append(dic)
-        if isEmptyOrNone(mode_dic[mode]):
-            mode_error_info.append(row + "类型是空的")
-        if isEmptyOrNone(brand_dic[brand]):
-            brand_error_info.append(row + '品牌是空的')
+        if isEmptyOrNone(mode) or isEmptyOrNone(brand):
+            mode_error_info.append(row + "类型或品牌是空的")
+        else:
+            if isEmptyOrNone(mode_dic[mode]):
+                mode_error_info.append(row + "类型是空的")
+            if isEmptyOrNone(brand_dic[brand]):
+                brand_error_info.append(row + '品牌是空的')
 
     if len(mode_error_info) > 0 or len(brand_error_info) > 0:
         logger.info("error--------物料品牌或者类型不能是空")
